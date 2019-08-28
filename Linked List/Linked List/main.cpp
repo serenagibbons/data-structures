a6dc86ad1900b4a022b20dca8ad744ee549a29a9
Serena Gibbons <serenalgibbons@gmail.com>
2019-08-27 22:15:29 -0400

    Add insert function

--- Linked List/Linked List/main.cpp
+++ Linked List/Linked List/main.cpp
@@ -6,9 +6,31 @@ struct Node {
 	Node *next;
 };
 
+Node *head;	// declare head node
+
 int main() {
 
 	system("pause");
 	return 0;
 }
 
+
+void insert(int x) {
+	if (head == NULL) {
+		Node *n = new Node;
+		n->data = x;
+		n->next = NULL;
+		head = n;
+	}
+	else {
+		Node *p;	// temporary pointer
+		p = head;
+		while (p->next != NULL) {	// while the node is not the tail
+			p = p->next;			// traverse the list
+		}
+		Node *n = new Node;
+		p->next = n;				// add the node to the linked list
+		n->data = x;
+		n->next = NULL;
+	}
+}
