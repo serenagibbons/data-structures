0ef67b984e28f32375c90bcfb5fc9f2034ff7923
Serena Gibbons <serenalgibbons@gmail.com>
2020-06-24 19:46:29 -0400

    Recursive binary search

---
+++ Algorithms/binarySearch/binarySearch/binarySearch.h
@@ -0,0 +1,10 @@
+#ifndef BINARYSEARCH_H
+#define BINARYSEARCH_H
+
+// iterative binary search
+int binarySearch(int[], const int SIZE, int target);
+
+// recursive binary search
+int binarySearch(int[], int l, int r, int target);
+
+#endif BINARYSEARCH_H
